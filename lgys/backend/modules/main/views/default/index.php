0dbcef27b63e49a5041ee26ada08d3284484016b
blinkccw <blink_ccw@163.com>
2020-03-16 15:13:52 +0800

    cjs second commit

--- lgys/backend/modules/main/views/default/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->title = '细物互享-运营管理平台';
-?>
-<?= Yii::$app->runAction('/main/default/welcome') ?>
-
--- lgys/backend/web/js/page/business/aggregation_man.js
+++
@@ -1,5 +0,0 @@
-var manPageIndex=1;
-//获取列表
-function getManList() {
-    $.LoadPage($('#dataManListBox'), '/business/page/aggregation-man-list', {"page_index":manPageIndex});
-}
